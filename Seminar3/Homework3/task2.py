a30a34bf2f9f58c0aa83651fabbe089ffb27684c
Dony-KG <dony.kg1@gmail.com>
2023-03-24 22:56:14 +0600

    изменено ДЗ №3, 3 семинара, игра скрабл

--- Seminar3/Homework3/task2.py
+++ Seminar3/Homework3/task2.py
@@ -6,37 +6,20 @@
 #  Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
 #  либо только русские буквы.
 
-list1 = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
-list2 = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
-list3 = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
-list4 = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
-list5 = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
-list8 = ['J', 'X', 'Ш', 'Э', 'Ю']
-list10 = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+alphabet = {1: 'AEIOULNSTRАВЕИНОРСТ',
+            2: 'DGДКЛМПУ',
+            3: 'BCMPБГЁЬЯ',
+            4: 'FHVWYЙЫ',
+            5: 'KЖЗХЦЧ',
+            8: 'JXШЭЮ',
+            10: 'QZФЩЪ'}
 
-count = 0
-
+point_word=0
 word = input("Введите слово: ")
-word = list(word.upper())
-for i in range(len(list1)):
-    count += word.count(list1[i])
+word = word.upper()
+for char in word:
+    for key, value in alphabet.items():
+        if char in value:
+            point_word+=key
 
-for i in range(len(list2)):
-    count += word.count(list2[i])*2
-
-for i in range(len(list3)):
-    count += word.count(list3[i])*3
-
-for i in range(len(list4)):
-    count += word.count(list4[i])*4
-
-for i in range(len(list5)):
-    count += word.count(list5[i])*5
-
-for i in range(len(list8)):
-    count += word.count(list8[i])*8
-
-for i in range(len(list10)):
-    count += word.count(list10[i])*10
-
-print(count)
+print(point_word)
