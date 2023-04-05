f4b34120cd9463540cccd9877047a97428bff355
Dony-KG <dony.kg1@gmail.com>
2023-04-05 20:34:39 +0600

    добавлена ДЗ №2, 6 семинара,добавлен еще оин вариант арифметической прогрессии

--- Seminar_6/Homework6/task1.py
+++ Seminar_6/Homework6/task1.py
@@ -7,8 +7,13 @@ length = int(input('Введите длину прогрессии: '))
 difference = int(input('Введите разность прогрессии: '))
 
 list_1 = []
+list_2 = []
 list_1.append(first)
 for i in range(1, length):
     list_1.append(list_1[i - 1] + difference)
 
 print(list_1)
+
+for i in range(length):
+    list_2.append(first + i * difference)
+print(list_2)
